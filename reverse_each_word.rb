29ca53f771ef831be5513cdf84a05c47ce8afe0f
Amy Westcott <amy.westcott@yahoo.com>
2019-06-04 18:41:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def reverse_each_word(string)
   array_word = string.split(" ")
-  array_word.each do |word|
+  array_word.collect do |word|
     word.reverse!
   end
  string = array_word.join(" ")
